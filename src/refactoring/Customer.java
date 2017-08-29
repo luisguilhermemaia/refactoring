f215ce24b8a9a938d822480bfc1b93cbb95ba3c4
Guilherme <guilherme@macbook-pro-2.local>
2017-08-29 16:51:08 -0300

    Removing Temps

---
+++ src/refactoring/Customer.java
@@ -0,0 +1,61 @@
+package refactoring;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Customer {
+
+	private String _name;
+	private Vector<Rental> _rentals = new Vector<Rental>();
+
+	public Customer(String name) {
+		_name = name;
+	};
+
+	public void addRental(Rental arg) {
+		_rentals.addElement(arg);
+	}
+
+	public String getName() {
+		return _name;
+	};
+
+	public String statement() {
+		Enumeration<Rental> rentals = _rentals.elements();
+		String result = "Rental Record for " + getName() + "\n";
+
+		while (rentals.hasMoreElements()) {
+			Rental each = rentals.nextElement();
+			//show figures for this rental
+			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+		}
+		// add footer lines
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+
+		result += "You earned " + String.valueOf(getFrequentPoints()) + " frequent renter points";
+
+		return result;
+
+	}
+	
+	private int getFrequentPoints(){
+		int frequentRenterPoints = 0;
+		Enumeration<Rental> rentals = _rentals.elements();
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			frequentRenterPoints = each.frequentRenterPoints();
+		}
+		return frequentRenterPoints;
+	}
+
+	private double getTotalCharge() {
+		double result = 0;
+		Enumeration<Rental> rentals = _rentals.elements();
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			result += each.getCharge();
+		}
+		return result;
+	}
+
+}
